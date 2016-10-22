244b689377a592769cfd3d1f250234bdd2cc034a
Mikko Eberhardt <mikko.eberhardt@haw-hamburg.de>
2016-10-22 17:25:41 +0200

    working but wrong printing

--- python/temperature.py
+++ python/temperature.py
@@ -17,7 +17,7 @@ else:
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-temperature = Adafruit_DHT.read_retry(sensor, pin)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -26,8 +26,8 @@ temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if temperature is not None :
-    print('Temp={0:0.1%f}*',temperature)
+if humidity is not None and temperature is not None :
+    print('Temp={0:0.1%f}*'.format(temperature, humidity)
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
