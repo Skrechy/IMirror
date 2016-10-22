3f9d4f065eec385d1e5f13412473ec8e3a1b3909
Mikko Eberhardt <mikko.eberhardt@haw-hamburg.de>
2016-10-22 17:21:12 +0200

    spelling error in temperature

--- python/temperature.py
+++ python/temperature.py
@@ -17,7 +17,7 @@ else:
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity = Adafruit_DHT.read_retry(sensor, pin)
+temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -26,7 +26,7 @@ humidity = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None :
+if temperature is not None :
     print('Temp={0:0.1f}*'.format(temperature))
 else:
     print('Failed to get reading. Try again!')
